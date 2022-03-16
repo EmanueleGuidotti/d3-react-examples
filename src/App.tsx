fb335bcc5bcead33b308c2e567410e4563030f79
emanuele.guidotti <guidotti.ema@gmail.com>
2022-03-16 11:12:02 +0100

    added tslint and improved code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Axis } from "./MolecularComponents/Axis/Axis";
+import * as d3 from "d3";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <svg width="800" height="400" id="svg">
+        <Axis
+          scaleFunction={d3
+            .scaleLinear()
+            .domain([0, 10])
+            .range([0, 200])}
+          axisFunction={d3.axisLeft}
+        ></Axis>
+      </svg>
+    </div>
   );
 }
 
